cc3bb9f43720176de57498eb88ecf68a8d2a21bd
ραяιкѕнιт <87859520+Parixshit@users.noreply.github.com>
2023-03-22 04:18:24 +0530

    Update
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-# Pornhub-Video-Downloader
-Pornhub视频下载Chrome插件
-
-通过向Pornhub视频页面注入JS获取视频接口信息，提取链接下载。
-
-## 2021年2月18日 更新v2.0版本，现已支持Xvideos视频下载！
-
-### 如果是Chrome (谷歌)浏览器，下载zip后解压加载
-
-本地加载插件的方法：
-
-url地址栏打开    chrome://extensions/
-![image](./img/usage.png)
-
-### chrome内核的浏览器（例如360,360极速这类浏览器）
-下载crx后拖入浏览器即可。
-
+# Pornhub Video Downloader - Chrome Extension
--- content-script.js
+++ content-script.js
@@ -1,84 +1,90 @@
 ﻿function Func() {
-	return new Promise((resolve, reject) => {
-		var jsString = document.querySelector("#player >script:nth-child(2)")
-		if (!jsString) {
-			jsString = document.querySelector("#player >script:nth-child(1)")
-		}
-		if (jsString) {
-			jsString = jsString.innerHTML
-			jsString = `	var playerObjList = {};\n${jsString}`
-			var flashvars = jsString.match("flashvars_[0-9]{1,}")[0]
-			eval(jsString)
-			var jsObject = eval(flashvars)
-			resolve(jsObject)
-		}
-		jsString = document.querySelector("#video-player-bg > script:nth-child(6)")
-		if (jsString) {
-			jsString = jsString.innerHTML
-			console.log(jsString)
-			var videoType = []
-			var urlTitle = jsString.match(/setVideoTitle\('(.*?)'\);/)[1]
-			var urlLow = jsString.match(/setVideoUrlLow\('(.*?)'\);/)[1]
-			if (urlLow){
-				var obj ={
-					key: "Low",
-					val: urlLow,
-					video_title: urlTitle
-				}
-				videoType.push(obj)
-			}
-			var urlHigh = jsString.match(/setVideoUrlHigh\('(.*?)'\);/)[1]
-			if (urlHigh){
-				var obj ={
-					key: "High",
-					val: urlHigh,
-					video_title: urlTitle
-				}
-				videoType.push(obj)
-			}
-			chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-			if (request.cmd == 'test') 
-				sendResponse(videoType);
-			});
-		}
-	})
-}
-
-function getMp4Url(url){
-    return new Promise((resolve, reject) => {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url , true);
-        xhr.onload = function(){
-            if (xhr.readyState === 4 && xhr.status === 200){
-                mp4UrlJson = JSON.parse(xhr.responseText);
-                resolve(mp4UrlJson);
-            } else {
-                reject(xhr.statusText);
-            }
+  return new Promise((resolve, reject) => {
+    var jsString = document.querySelector("#player >script:nth-child(2)");
+    if (!jsString) {
+      jsString = document.querySelector("#player >script:nth-child(1)");
+    }
+    if (jsString) {
+      jsString = jsString.innerHTML;
+      jsString = `	var playerObjList = {};\n${jsString}`;
+      var flashvars = jsString.match("flashvars_[0-9]{1,}")[0];
+      eval(jsString);
+      var jsObject = eval(flashvars);
+      resolve(jsObject);
+    }
+    jsString = document.querySelector("#video-player-bg > script:nth-child(6)");
+    if (jsString) {
+      jsString = jsString.innerHTML;
+      console.log(jsString);
+      var videoType = [];
+      var urlTitle = jsString.match(/setVideoTitle\('(.*?)'\);/)[1];
+      var urlLow = jsString.match(/setVideoUrlLow\('(.*?)'\);/)[1];
+      if (urlLow) {
+        var obj = {
+          key: "Low",
+          val: urlLow,
+          video_title: urlTitle,
         };
-        xhr.send();
-    });
+        videoType.push(obj);
+      }
+      var urlHigh = jsString.match(/setVideoUrlHigh\('(.*?)'\);/)[1];
+      if (urlHigh) {
+        var obj = {
+          key: "High",
+          val: urlHigh,
+          video_title: urlTitle,
+        };
+        videoType.push(obj);
+      }
+      chrome.runtime.onMessage.addListener(function (
+        request,
+        sender,
+        sendResponse
+      ) {
+        if (request.cmd == "test") sendResponse(videoType);
+      });
+    }
+  });
 }
 
-Func().then(res => {
-	var videoType = []
-	Object.keys(res['mediaDefinitions']).forEach((item) => {
-		var itemInfo = res['mediaDefinitions'][item]
-		if (itemInfo['format'] == 'mp4') {
-			getMp4Url(itemInfo['videoUrl']).then(mp4UrlJson => {
-				for (const mp4Url of mp4UrlJson){
-					var obj = {
-					key: mp4Url['quality'],
-					val: mp4Url['videoUrl'],
-					video_title:res.video_title
-					}
-				videoType.push(obj)
-				}
-			})
-		}
-	})
-	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-		if (request.cmd == 'test') 
-			sendResponse(videoType);
-	});
+function getMp4Url(url) {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        mp4UrlJson = JSON.parse(xhr.responseText);
+        resolve(mp4UrlJson);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.send();
+  });
+}
+
+Func().then((res) => {
+  var videoType = [];
+  Object.keys(res["mediaDefinitions"]).forEach((item) => {
+    var itemInfo = res["mediaDefinitions"][item];
+    if (itemInfo["format"] == "mp4") {
+      getMp4Url(itemInfo["videoUrl"]).then((mp4UrlJson) => {
+        for (const mp4Url of mp4UrlJson) {
+          var obj = {
+            key: mp4Url["quality"],
+            val: mp4Url["videoUrl"],
+            video_title: res.video_title,
+          };
+          videoType.push(obj);
+        }
+      });
+    }
+  });
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.cmd == "test") sendResponse(videoType);
+  });
 });
